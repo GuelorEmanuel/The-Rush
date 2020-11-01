fca9dda3777b0b1beb84508b7539bc78c5c62bf6
Guelor Emanuel <gueloremanuel@cmail.carleton.ca>
2020-11-01 12:42:27 -0500

    Set Fomantic UI

--- lib/the_rush_web/endpoint.ex
+++ lib/the_rush_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule TheRushWeb.Endpoint do
     at: "/",
     from: :the_rush,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt vendor)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
