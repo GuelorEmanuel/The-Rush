bbb0cd9f77f85fdd10f40e4acdae7368bbf26dcc
Guelor Emanuel <gueloremanuel@cmail.carleton.ca>
2020-11-02 16:23:47 -0500

    Add distillery

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule TheRush.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:distillery, "~> 2.1"}
     ]
   end
 
