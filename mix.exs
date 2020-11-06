a8bcca167c399d45c911c99d5894026d974185eb
Guelor Emanuel <gueloremanuel@cmail.carleton.ca>
2020-11-06 04:47:37 -0500

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -44,7 +44,10 @@ defmodule TheRush.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.1"}
+      {:distillery, "~> 2.1"},
+      {:mongodb, "~> 0.5.1"},
+      {:csv, "~> 2.4"},
+      {:con_cache, "~> 0.14.0"}
     ]
   end
 
