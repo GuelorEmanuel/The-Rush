e2149f6215fc0272632823e3a2ab123fbba6a16a
Guelor Emanuel <gueloremanuel@cmail.carleton.ca>
2020-11-06 06:23:55 -0500

    Revert config

--- config/test.exs
+++ config/test.exs
@@ -3,8 +3,9 @@ use Mix.Config
 # Configure your database
 config :the_rush, :db_config,
   name: :mongo,
-  url: "mongodb+srv://guelor:#{System.get_env("MONGO_DB_PASSWORD")}@cluster0.4j8hm.mongodb.net/#{System.get_env("MONGO_CLUSTER_NAME")}?retryWrites=true&w=majority",
-  pool_size: 2
+  database: "test",
+  seeds: ["mongodb:27017"],
+  pool: DBConnection.Poolboy
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
