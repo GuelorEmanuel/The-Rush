64c84dffc44c9bc610395a5c1e90eaf23a0b5665
Guelor Emanuel <gueloremanuel@cmail.carleton.ca>
2020-11-06 04:55:29 -0500

    Add get request path for csv download

--- lib/the_rush_web/router.ex
+++ lib/the_rush_web/router.ex
@@ -18,6 +18,7 @@ defmodule TheRushWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    get "/csv", CsvController, :export
   end
 
   # Other scopes may use custom stacks.
