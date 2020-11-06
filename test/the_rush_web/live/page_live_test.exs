f4b0c3d7091646259a6ea1b23ea31627feba07c5
Guelor Emanuel <gueloremanuel@cmail.carleton.ca>
2020-11-06 05:05:14 -0500

    Update test

--- test/the_rush_web/live/page_live_test.exs
+++ test/the_rush_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule TheRushWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "the Rush"
+    assert render(page_live) =~ "NFL Rushing Stat"
   end
 end
